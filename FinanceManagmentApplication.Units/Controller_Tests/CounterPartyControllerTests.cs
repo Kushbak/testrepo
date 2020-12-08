cfc2d1aa2b9ce3e7689a66feada1b1b5e7db2888
Your Name <rassarassa8@gmail.com>
2020-12-08 08:17:20 +0600

    UnitsTesting

--- FinanceManagmentApplication.Units/Controller_Tests/CounterPartyControllerTests.cs
+++ FinanceManagmentApplication.Units/Controller_Tests/CounterPartyControllerTests.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using FinanceManagmentApplication.BL.Services.Contracts;
+using FinanceManagmentApplication.Controllers;
+using FinanceManagmentApplication.Models.CounterPartiesModel;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace FinanceManagmentApplication.Units.Controller_Tests
 {
-    class CounterPartyControllerTests
+    public class CounterPartyControllerTests
     {
+        [Fact]
+        public async Task CounterPartyIndexTest()
+        {
+            var mock = new Mock<ICounterPartyService>();
+            mock.Setup(repo => repo.GetAll()).Returns(GetTestCounterPartiesIndexModels());
+            var controller = new CounterPartyController(mock.Object);
+
+            // Act
+            var result = await controller.Index();
+            var testResult = await  GetTestCounterPartiesIndexModels();
+            // Assert
+            var viewResult = Assert.IsType<ActionResult<List<CounterPartyIndexModel>>>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<CounterPartyIndexModel>>(viewResult.Value);
+            Assert.Equal( testResult.Count, model.Count());
+        }
+
+        private async Task<List<CounterPartyIndexModel>> GetTestCounterPartiesIndexModels()
+        {
+            return new List<CounterPartyIndexModel>
+            {
+                new CounterPartyIndexModel{Id = 1 },
+                new CounterPartyIndexModel {Id = 2}
+            };
+        }
     }
+    
 }
--- FinanceManagmentApplication.Units/Controller_Tests/ProjectControllerTests.cs
+++ FinanceManagmentApplication.Units/Controller_Tests/ProjectControllerTests.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using FinanceManagmentApplication.BL.Services.Contracts;
+using FinanceManagmentApplication.Controllers;
+using FinanceManagmentApplication.Models.ProjectModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace FinanceManagmentApplication.Units.Controller_Tests
 {
-    class ProjectControllerTests
+    public class ProjectControllerTests
     {
+        [Fact]
+        public async Task ProjectIndexTest()
+        {
+            var mock = new Mock<IProjectService>();
+            mock.Setup(repo => repo.GetAll()).Returns(GetProjectIndexModels());
+            var controller = new ProjectController(mock.Object);
+
+            // Act
+            var result = await controller.Get();
+            var testResult = await GetProjectIndexModels();
+            // Assert
+            var viewResult = Assert.IsType<ActionResult<List<ProjectIndexModel>>>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<ProjectIndexModel>>(viewResult.Value);
+            Assert.Equal(testResult.Count, model.Count());
+        }
+
+        private async Task<List<ProjectIndexModel>> GetProjectIndexModels()
+        {
+            return new List<ProjectIndexModel>()
+                { 
+                    new ProjectIndexModel {Id = 1},
+                    new ProjectIndexModel {Id = 2}
+                };
+        }
     }
 }
